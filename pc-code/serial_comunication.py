8dfab010b3c52d6ef76c23f61c3c0023ad9d1f9c
diegobenavides <diegob41@gmail.com>
2014-04-23 17:53:56 -0600

    adding code for serial communication with the arduino

---
+++ pc-code/serial_comunication.py
@@ -0,0 +1,33 @@
+import serial
+import time
+import sys
+
+class arduinoCom(object):
+	def __init__(self):
+		try:
+			for device in range(0,5):
+				self.arduino = serial.Serial("/dev/ttyACM%i"+device,9600)
+				self.arduino.timeout = 1
+				print("connected through %s", device)
+				time.sleep(1)
+				break
+		except:
+			print("Can not connect to arduino device")
+			sys.exit(-1)
+	
+	def write(self, message):
+		 self.arduino.write(str(message))
+	
+def main():
+	test = arduinoCom()
+	test.write("f")
+	time.sleep(1)
+	test.write("r")
+	time.sleep(1)
+	test.write("l")
+	time.sleep(1)
+	return 0
+
+if __name__ == '__main__':
+	main()
+
