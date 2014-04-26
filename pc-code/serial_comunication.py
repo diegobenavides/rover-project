5e350ed0308ff8d63ea779d81e53178a3a14bb24
jvaldes <jorge.d.v.m@hotmail.com>
2014-04-25 19:18:26 -0600

    Making changes in rover proyect and arduino code to center the drone an move it

--- pc-code/serial_comunication.py
+++ pc-code/serial_comunication.py
@@ -5,27 +5,49 @@ import sys
 class arduinoCom(object):
 	def __init__(self):
 		try:
-			for device in range(0,5):
-				self.arduino = serial.Serial("/dev/ttyACM%i" %device,9600)
-				print("connected through /dev/ttyACM%i" %device)
-				break
+			self.arduino = serial.Serial("/dev/ttyACM0" ,9600)
+			print("Connected to Arduino")	
 		except:
 			print("Can not connect to arduino device")
 			sys.exit(-1)
-	
+			#time.sleep(0.5)
+			
 	def write(self, message):
 		 self.arduino.write(str(message))
+		 
+	def readline_A(self):
+		 self.arduino.readline()
+	
 	
 def main():
+	
 	test = arduinoCom()
-	test.write("f")
-	time.sleep(1)
+	
+	print "Here"
+	while True:
+		print test.readline_A()
+	"""	
+	#print "Here ",test.arduino.readline()
+	print "Reading... ",test.arduino.readline()
+	#test.write("R")
+		
+	print "Here ",test.arduino.readline()
+	print "Reading... ",test.arduino.readline()
+	test.write("R")
+	print "1",test.arduino.readline()
+	print "2",test.arduino.readline()
+	print "3",test.arduino.readline()
+	print "4",test.arduino.readline()
+	print "5",test.arduino.readline()
+		
+	
 	test.write("r")
 	time.sleep(1)
-	test.write("l")
+	test.write("b")
 	time.sleep(1)
+	"""
 	return 0
-
+	
 if __name__ == '__main__':
 	main()
 
